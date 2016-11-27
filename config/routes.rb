011ede975114f9b4c9a8613b3b763cc6971f5118
kangsooj <skang17@berkeley.edu>
2016-11-27 20:36:36 +0000

    added routes, started nav

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'shop' => 'shop#pagecontainer'
+  get 'main' => 'shop#main'
+  get 'about' => 'shop#about'
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
